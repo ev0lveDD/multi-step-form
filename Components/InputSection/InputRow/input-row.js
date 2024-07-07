419fa7a6321be8a54a61fea47bc4591b43399419
Jagiela <kjagiela93@gmail.com>
2024-07-07 17:17:05 +0200

    fixed Tailwind CSS styles for items

--- Components/InputSection/InputRow/input-row.js
+++ Components/InputSection/InputRow/input-row.js
@@ -4,7 +4,7 @@ export default function InputRow({inputLabel, inputPlaceholder, inputId, inputTy
             <label className="block tracking-wide text-slate-800 text-[10px] font-semibold py-1">
                 {inputLabel}
             </label>
-            <input className="block w-full text-black text-xs p-2 font-semibold bg-white border border-gray-300 rounded-md focus:outline-none focus:border-violet-800" 
+            <input className="block w-full text-black text-xs p-2 font-semibold bg-white border border-gray-300 rounded-md focus:outline-none focus:border-violet-800 hover:border-violet-800 cursor-pointer" 
                 id={inputId}
                 type={inputType}
                 placeholder={inputPlaceholder}>
