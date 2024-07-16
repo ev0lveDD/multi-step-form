d096f26b51962d01e4293764fae365d8fb6c5ffc
Jagiela <kjagiela93@gmail.com>
2024-07-16 16:36:54 +0200

    Fixed wrapper width and height, fixed menu width and height for responsivnes

--- Components/CheckboxSection/CheckboxButton/checkbox-button.js
+++ Components/CheckboxSection/CheckboxButton/checkbox-button.js
@@ -1,8 +1,8 @@
 export default function CheckboxButton({id, addonName, addonDescription, addonPrice, yearlyPlanBiling, hookValue, hookFunction}){
     return(
         <li className="flex items-center w-full relative">
-            <input type="checkbox" id={id} value="" className="m-6 peer z-10" required="" checked={hookValue} onChange={()=>hookFunction(!hookValue)}></input>
-            <label for={id} className="py-3 pl-4 md:pl-6 pr-1 z-0 absolute inline-flex items-center justify-between w-full p-5 bg-white border border-gray-300 rounded-md cursor-pointer hover:border-violet-800 peer-checked:border-violet-800 peer-checked:bg-slate-100">                           
+            <input type="checkbox" id={id} value="" className="my-6 mx-4 md:mx-6 peer z-10" required="" checked={hookValue} onChange={()=>hookFunction(!hookValue)}></input>
+            <label for={id} className="py-3 pl-2 md:pl-6 pr-1 z-0 absolute inline-flex items-center justify-between w-full p-5 bg-white border border-gray-300 rounded-md cursor-pointer hover:border-violet-800 peer-checked:border-violet-800 peer-checked:bg-slate-100">                           
                 <div className="flex items-center justify-between w-full pl-8 pr-2 md:pr-4"> 
                     <div className="flex flex-col">
                         <p className="text-sky-950 text-xs md:text-sm font-bold">{addonName}</p>
--- Components/Menu/menu.js
+++ Components/Menu/menu.js
@@ -2,7 +2,7 @@ import MenuRow from "./MenuRow/menu-row";
 
 export default function Menu({currentStep}){
     return(
-        <div className="h-40 md:h-96 w-full md:w-1/4 bg-pattern-desktop bg-auto md:bg-cover bg-center md:rounded m-0 md:m-4 flex md:flex-col justify-center md:justify-start gap-3 md:gap-0">
+        <div className="h-40 md:h-96 w-full md:w-1/3 lg:w-1/4 bg-pattern-desktop bg-auto md:bg-cover bg-center md:rounded m-0 md:m-4 flex md:flex-col justify-center md:justify-start gap-3 md:gap-0">
             <MenuRow number={1} description={"YOUR INFO"} active={currentStep === "First" ? true : false}/>
             <MenuRow number={2} description={"SELECT PLAN"} active={currentStep === "Second" ? true : false}/>
             <MenuRow number={3} description={"ADD-ONS"} active={currentStep === "Third" ? true : false}/>
